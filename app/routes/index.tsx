683df3045112946d52c9afc31f9f5fe07c6bfee1
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-02-08 01:59:44 +0100

    Update index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
+import { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return { title: "PizzaFahrer" };
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="p-8 mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+      <h1 className="text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+         {'🚗💨 Pizza'}
+        <span className="text-red-600">Fahrer</span>
+      </h1>
     </div>
   );
 }
